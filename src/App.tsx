fd2238d11c2c687acc34df757c26f2e50631b011
chiagbanweghi101 <c.peter@alustudent.com>
2026-09-22 21:46:03 +0200

    feat: add App root component

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Header from './components/Header';
+import PostList from './components/PostList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main>
+        <PostList />
+      </main>
+    </div>
+  );
+}
+
+export default App;
